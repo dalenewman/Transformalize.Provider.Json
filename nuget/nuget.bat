342c6b6aa3717f08a8481f0cb09d46bca1df67ef
Dale Newman <dalenewman@gmail.com>
2019-09-27 11:53:51 -0400

    update to fromjson transform to respect root property

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -7,5 +7,5 @@ nuget pack Transformalize.Transform.Json.Autofac.nuspec -OutputDirectory "c:\tem
 REM nuget push "c:\temp\modules\Transformalize.Provider.Json.0.6.12-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Provider.Json.Autofac.0.6.12-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Provider.Json.Autofac.v3.0.6.12-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Json.0.6.12-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Json.Autofac.0.6.12-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Json.0.6.19-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Json.Autofac.0.6.19-beta.nupkg" -source https://api.nuget.org/v3/index.json
--- src/Transformalize.Transform.Json/FromJsonTransform.cs
+++ src/Transformalize.Transform.Json/FromJsonTransform.cs
@@ -24,85 +24,123 @@ using Transformalize.Contracts;
 
 namespace Transformalize.Transforms.Json {
 
-    public class FromJsonTransform : BaseTransform {
-        private readonly Func<object, string> _serializer;
-        private readonly Field _input;
-        private readonly Field[] _output;
-        private readonly HashSet<string> _errors = new HashSet<string>();
+   public class FromJsonTransform : BaseTransform {
+      private readonly Func<object, string> _serializer;
+      private readonly Field _input;
+      private readonly Field[] _output;
+      private readonly HashSet<string> _errors = new HashSet<string>();
 
-        public FromJsonTransform(IContext context = null, Func<object, string> serializer = null) : base(context, "object") {
+      public FromJsonTransform(IContext context = null, Func<object, string> serializer = null) : base(context, "object") {
 
-            ProducesFields = true;
+         ProducesFields = true;
 
-            if (IsMissingContext()) {
-                return;
+         if (IsMissingContext()) {
+            return;
+         }
+
+         if (!Context.Operation.Parameters.Any()) {
+            Error($"The {Context.Operation.Method} transform requires a collection of output fields.");
+            Run = false;
+            return;
+         }
+
+         if (serializer == null) {
+            Error($"The {Context.Operation.Method} transform requires a JSON serializer.");
+            Run = false;
+            return;
+         }
+
+         _serializer = serializer;
+         _input = SingleInputForMultipleOutput();
+         _output = MultipleOutput();
+      }
+
+      public override IRow Operate(IRow row) {
+         var json = (string)row[_input];
+
+         try {
+            var dict = JSON.Parse(json) as Dictionary<string, object>;
+
+            if (Context.Operation.Root != string.Empty) {
+               dict = FindKey(dict, Context.Operation.Root);
             }
 
-            if (!Context.Operation.Parameters.Any()) {
-                Error($"The {Context.Operation.Method} transform requires a collection of output fields.");
-                Run = false;
-                return;
-            }
-
-            if(serializer == null) {
-                Error($"The {Context.Operation.Method} transform requires a JSON serializer.");
-                Run = false;
-                return;
-            }
-
-            _serializer = serializer;
-            _input = SingleInputForMultipleOutput();
-            _output = MultipleOutput();
-        }
-
-        public override IRow Operate(IRow row) {
-            var json = (string)row[_input];
-
-            try {
-                var dict = JSON.Parse(json) as Dictionary<string, object>;
-                if (dict != null) {
-                    if (_output.Any() && !_output[0].Equals(_input)) {
-                        foreach (var field in _output) {
-                            if (dict.ContainsKey(field.Name)) {
-                                var value = dict[field.Name];
-                                if (value is string) {
-                                    if (field.DefaultEmpty && value.Equals(string.Empty)) {
-                                        row[field] = field.Default;
-                                    } else {
-                                        row[field] = value;
-                                    }
-                                } else if (value is int || value is long || value is double) {
-                                    row[field] = value;
-                                } else {
-                                    row[field] = _serializer(value);
-                                }
-                            }
+            if (dict != null) {
+               if (_output.Any() && !_output[0].Equals(_input)) {
+                  foreach (var field in _output) {
+                     if (dict.ContainsKey(field.Name)) {
+                        var value = dict[field.Name];
+                        if (value is string) {
+                           if(field.Type == "string") {
+                              if (field.DefaultEmpty && value.Equals(string.Empty)) {
+                                 row[field] = field.Default;
+                              } else {
+                                 row[field] = value;
+                              }
+                           } else {
+                              row[field] = field.Convert(value);
+                           }
+                        } else if (value is bool || value is int || value is long || value is double) {
+                           row[field] = value;
+                        } else { // array or object
+                           row[field] = _serializer(value);
                         }
-                    } else {
-                        if (dict.ContainsKey(Context.Field.Name)) {
-                            var value = dict[Context.Field.Name];
-                            if (value is string || value is int || value is long || value is double) {
-                                row[Context.Field] = value;
-                            } else {
-                                row[Context.Field] = _serializer(value);
-                            }
+                     }
+                  }
+               } else {
+                  if (dict.ContainsKey(Context.Field.Name)) {
+                     var value = dict[Context.Field.Name];
+                     if(value is string) {
+                        if (Context.Field.Type == "string") {
+                           if (Context.Field.DefaultEmpty && value.Equals(string.Empty)) {
+                              row[Context.Field] = Context.Field.Default;
+                           } else {
+                              row[Context.Field] = value;
+                           }
+                        } else {
+                           row[Context.Field] = Context.Field.Convert(value);
                         }
-                    }
-                }
-            } catch (Exception ex) {
-                if (_errors.Add(ex.Message)) {
-                    Context.Error(ex, ex.Message);
-                }
+                     } else if (value is bool || value is int || value is long || value is double) {
+                        row[Context.Field] = value;
+                     } else {  // array or object
+                        row[Context.Field] = _serializer(value);
+                     }
+                  }
+               }
             }
+         } catch (Exception ex) {
+            if (_errors.Add(ex.Message)) {
+               Context.Error(ex, ex.Message);
+            }
+         }
 
 
-            return row;
+         return row;
 
-        }
+      }
 
-        public override IEnumerable<OperationSignature> GetSignatures() {
-            return new[] { new OperationSignature("fromjson") };
-        }
-    }
+      public override IEnumerable<OperationSignature> GetSignatures() {
+         return new[] { new OperationSignature("fromjson") };
+      }
+
+      public Dictionary<string,object> FindKey(Dictionary<string, object> dict, string search) {
+         if (dict.ContainsKey(search)) {
+            return dict[search] as Dictionary<string, object>;
+         } else {
+            foreach (string key in dict.Keys) {
+               object nested = dict[key];
+               if (nested == null) {
+                  continue;
+               }
+               if(nested is Dictionary<string, object> x) {
+                  return FindKey(x, search);
+               }
+               
+            }
+         }
+         return null;
+      }
+
+   }
 
 }
